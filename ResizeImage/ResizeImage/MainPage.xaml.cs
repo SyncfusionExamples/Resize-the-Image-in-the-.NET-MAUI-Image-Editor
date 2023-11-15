39b3998140774c6bfd3414a67edc528b8e1f8477
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2023-11-15 18:46:56 +0530

    Sample added.

---
+++ ResizeImage/ResizeImage/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ResizeImage;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+    private void OnButtonClicked(object sender, EventArgs e)
+    {
+        this.imageEditor.Save(imageSize: new Size(200, 200));
+    }
+}
